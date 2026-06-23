cb7815b5d143e771f7fb6f3daf7238425174692e
Sumit Yadav <sumitkumaryadav1810@gmail.com>
2026-06-23 09:33:21 +0530

    Time: 1 ms (93.80%) | Memory: 43.2 MB (84.53%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,32 +1,22 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> result = new ArrayList<>();
-        if (numRows == 0) {
-            return result;
+        List<List<Integer>> res = new ArrayList<>();
+        res.add(List.of(1));
+
+        for (int i = 0; i < numRows - 1; i++) {
+            List<Integer> dummyRow = new ArrayList<>();
+            dummyRow.add(0);
+            dummyRow.addAll(res.get(res.size() - 1));
+            dummyRow.add(0);
+            List<Integer> row = new ArrayList<>();
+
+            for (int j = 0; j < dummyRow.size() - 1; j++) {
+                row.add(dummyRow.get(j) + dummyRow.get(j + 1));
+            }
+
+            res.add(row);
         }
 
-        if (numRows == 1) {
-            List<Integer> firstRow = new ArrayList<>();
-            firstRow.add(1);
-            result.add(firstRow);
-            return result;
-        }
-
-        result = generate(numRows - 1);
-        List<Integer> prevRow = result.get(numRows - 2);
-        List<Integer> currentRow = new ArrayList<>();
-        currentRow.add(1);
-
-        for (int i = 1; i < numRows - 1; i++) {
-            currentRow.add(prevRow.get(i - 1) + prevRow.get(i));
-        }
-
-        currentRow.add(1);
-        result.add(currentRow);
-
-        return result;
+        return res;        
     }
 }
