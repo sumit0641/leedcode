fbe4f7d58b02e5409b7b89a093f25d9563824d8d
Sumit Yadav <sumitkumaryadav1810@gmail.com>
2026-06-29 11:39:28 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (8.19%) - LeetSync

--- 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.java
+++ 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.java
@@ -1,43 +1,8 @@
 class Solution {
-
     public int numOfStrings(String[] patterns, String word) {
-        int res = 0;
-        for (String pattern : patterns) {
-            if (check(pattern, word)) {
-                res++;
-            }
-        }
-        return res;
-    }
-
-    private boolean check(String pattern, String word) {
-        int m = pattern.length();
-        int n = word.length();
-
-        // generate the prefix array of the pattern
-        int[] pi = new int[m];
-        for (int i = 1, j = 0; i < m; i++) {
-            while (j > 0 && pattern.charAt(i) != pattern.charAt(j)) {
-                j = pi[j - 1];
-            }
-            if (pattern.charAt(i) == pattern.charAt(j)) {
-                j++;
-            }
-            pi[i] = j;
-        }
-
-        // using prefix arrays for matching
-        for (int i = 0, j = 0; i < n; i++) {
-            while (j > 0 && word.charAt(i) != pattern.charAt(j)) {
-                j = pi[j - 1];
-            }
-            if (word.charAt(i) == pattern.charAt(j)) {
-                j++;
-            }
-            if (j == m) {
-                return true;
-            }
-        }
-        return false;
+        int count = 0;
+        for (String s : patterns)
+            if (word.indexOf(s) != -1) count++; // -1 when not found
+        return count;
     }
 }
