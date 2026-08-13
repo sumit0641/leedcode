ac3cecc6bfb824b99269cb8c06c899a9c3e4cfc1
Sumit Yadav <sumitkumaryadav1810@gmail.com>
2026-08-13 14:08:01 +0530

    Time: 437 ms (17.14%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,5 +1,4 @@
 # Write your MySQL query statement below
-# Write your MySQL query statement below
 select case when id % 2 =1 and id+1 in (select id from Seat) then id+1
             when id % 2 =0 then id-1
             else id
